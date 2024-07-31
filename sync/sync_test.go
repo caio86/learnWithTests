95d591431663d1335bd74a4734e43997afb28115
caio86 <caioluiz86@gmail.com>
2024-07-31 15:27:36 -0300

    create sync package

---
+++ sync/sync_test.go
@@ -0,0 +1,25 @@
+package sync
+
+import (
+	"testing"
+)
+
+func TestCounter(t *testing.T) {
+
+	t.Run("incrementing the counter 3 times leaving it at 3", func(t *testing.T) {
+		counter := Counter{}
+		counter.Inc()
+		counter.Inc()
+		counter.Inc()
+
+		assertCounter(t, counter, 3)
+	})
+}
+
+func assertCounter(t testing.TB, got Counter, want int) {
+	t.Helper()
+
+	if got.Value() != want {
+		t.Errorf("got %d, want %d", got.Value(), want)
+	}
+}
